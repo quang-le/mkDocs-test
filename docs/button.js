74aa43d80c518ec802afedddcabda71b421fde85
Quang Son Le <qle@bluesquarehub.com>
2023-04-25 15:56:02 +0200

    Add custom button with js

---
+++ docs/button.js
@@ -0,0 +1,5 @@
+const footer = document.getElementsByTagName("footer")[0]
+const button = document.createElement("button")
+button.textContent="Toggle LOCALE"
+footer.appendChild(button)
+console.log("there there")
