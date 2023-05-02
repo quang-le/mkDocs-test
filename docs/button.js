76844ab721c89e63da9db22d9acdb17f1db86ed9
Quang Son Le <qle@bluesquarehub.com>
2023-05-02 17:20:56 +0200

    FIX: use button.js to toggle language

--- docs/button.js
+++ docs/button.js
@@ -1,5 +1,20 @@
 const footer = document.getElementsByTagName("footer")[0]
 const button = document.createElement("button")
-button.textContent="Toggle LOCALE"
+button.id = "toggleLanguage"
+const pathname = window.location.pathname
+console.log("PATH", pathname)
+button.textContent = pathname === '/fr/' ? "English" : "Français"
+button.onclick = ()=>{
+    if (pathname === "/fr/"){
+        const newLocation = window.location.href.replace(pathname,'/en/')
+        window.location.replace(newLocation) 
+    } else if (pathname === "/"){
+        const newLocation = window.location.href.concat('fr/')
+        window.location.replace(newLocation) 
+    }else {
+        const newLocation = window.location.href.replace(pathname,'/fr/')
+        window.location.replace(newLocation) 
+    }
+}
+
 footer.appendChild(button)
-console.log("there there")
